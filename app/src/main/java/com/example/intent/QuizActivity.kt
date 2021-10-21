cbef388f5501bb229b78dc77b2a21dabef2d6063
sukakku159 <tuanhiepri12@gmail.com>
2021-10-21 17:01:22 +0700

    update

--- app/src/main/java/com/example/intent/QuizActivity.kt
+++ app/src/main/java/com/example/intent/QuizActivity.kt
@@ -8,6 +8,10 @@ import kotlin.math.log
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQuizBinding
+    var mCurrentPosition : Int = 1
+    var mQuestionList : ArrayList<quizData>? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
@@ -17,8 +21,21 @@ class QuizActivity : AppCompatActivity() {
 
         val quizQuestion = Constants.getQuestion()
         Log.i("question size",  "${quizQuestion.size}")
+        mQuestionList = Constants.getQuestion()
+        setQuestion()
 
 
+    }
+    private fun setQuestion() {
+        mCurrentPosition = 1
+        val question = mQuestionList!![mCurrentPosition - 1]
+        binding.txQuestionNumber.text = "Question $mCurrentPosition"
+        binding.txQuestion.text = question.question
+        binding.imgQuestionImage.setImageResource(question.image)
+        binding.btnOptA.text = question.optA
+        binding.btnOptB.text = question.optB
+        binding.btnOptC.text = question.optC
+        binding.btnOptD.text = question.optD
 
     }
 }
