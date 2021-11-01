385f3b2568d07bfb0e88ac63a3d11ec24fe67a08
sukakku159 <tuanhiepri12@gmail.com>
2021-11-01 10:23:55 +0700

    main finished

--- app/src/main/java/com/example/intent/Constants.kt
+++ app/src/main/java/com/example/intent/Constants.kt
@@ -13,7 +13,112 @@ object Constants {
             "Ronaldo Aparecido Rodrigues",
             2
                 )
+        val question2 = quizData(
+            2,
+            "What is his name?",
+            R.drawable.batistuta,
+            "Gabriel Batistuta",
+            "Luca Toni",
+            "David Trezeguet",
+            "Diego Forlan",
+            1
+        )
+        val question3 = quizData(
+            3,
+            "What is his name?",
+            R.drawable.samuel,
+            "Esteban Cambiasso",
+            "Javier Zanetti",
+            "Antonio Conte",
+            "Walter Samuel",
+            4
+        )
+        val question4 = quizData(
+            4,
+            "What is his name?",
+            R.drawable.serginho,
+            "Marek Jankulovski",
+            "Cafu",
+            "Serginho",
+            "Camoranesi",
+            3
+        )
+        val question5 = quizData(
+            5,
+            "What is his name?",
+            R.drawable.piszczek,
+            "Lukas Piszczek",
+            "Nuri Sahin",
+            "Kevin Kampl",
+            "Wout Weghorst",
+            1
+        )
+        val question6 = quizData(
+            6,
+            "What is his name?",
+            R.drawable.cavani_palermo,
+            "Javier Pastore",
+            "Fabio Quagliarella",
+            "Gonzalo Higuain",
+            "Edison Cavani",
+            4
+        )
+        val question7 = quizData(
+            7,
+            "What is his name?",
+            R.drawable.camoranesi,
+            "Mauro Camoranesi",
+            "Mauro Icardi",
+            "Pablo Sarabia",
+            "Hakan Calhanoglu",
+            1
+        )
+        val question8 = quizData(
+            8,
+            "What is his name?",
+            R.drawable.benyedder,
+            "Wissam Ben Yedder",
+            "Gabigol",
+            "Myron Boadu",
+            "Ismael Bennacer",
+            1
+        )
+        val question9 = quizData(
+            9,
+            "What is his name",
+            R.drawable.forlan,
+            "Gonzalo Higuain",
+            "Ruud Van Nistelrooy",
+            "Thierry Henry",
+            "Diego Forlan",
+            4
+        )
+        val question10 = quizData(
+            10,
+            "What is his name",
+            R.drawable.veron,
+            "Andreas Iniesta",
+            "Paul Scholes",
+            "Diego Simeone",
+            "Veron",
+            4
+        )
+
+
+
         questionList.add(question1)
+        questionList.add(question2)
+        questionList.add(question3)
+        questionList.add(question4)
+        questionList.add(question5)
+        questionList.add(question6)
+        questionList.add(question7)
+        questionList.add(question8)
+        questionList.add(question9)
+        questionList.add(question10)
+
+
+
         return questionList
     }
 }
--- app/src/main/java/com/example/intent/QuizActivity.kt
+++ app/src/main/java/com/example/intent/QuizActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.intent.databinding.ActivityQuizBinding
 import kotlin.math.log
@@ -17,6 +18,7 @@ lateinit var binding: ActivityQuizBinding
     var mCurrentPosition : Int = 1
     var mQuestionList : ArrayList<quizData>? = null
     var mSelectedOptionPosition : Int = 0
+    var buttonCheckSelected : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,40 +31,77 @@ lateinit var binding: ActivityQuizBinding
         Log.i("question size",  "${quizQuestion.size}")
         mQuestionList = Constants.getQuestion()
         setQuestion()
+
+        binding.btnSubmit.isEnabled= false
+
         binding.btnOptA.setOnClickListener {
+            binding.btnSubmit.isEnabled = true
             defaultOptionView()
+
             binding.btnOptA.background = ContextCompat.getDrawable(this, R.drawable.btn_option_selected)
             binding.btnOptA.setTextColor(ContextCompat.getColor(this, R.color.black))
-
+            mSelectedOptionPosition =1
 
         }
         binding.btnOptB.setOnClickListener {
+            binding.btnSubmit.isEnabled = true
             defaultOptionView()
+            mSelectedOptionPosition = 2
             binding.btnOptB.background =
                 ContextCompat.getDrawable(this, R.drawable.btn_option_selected)
             binding.btnOptB.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
         binding.btnOptC.setOnClickListener {
+            binding.btnSubmit.isEnabled = true
             defaultOptionView()
+            mSelectedOptionPosition = 3
             binding.btnOptC.background =
                 ContextCompat.getDrawable(this, R.drawable.btn_option_selected)
             binding.btnOptC.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
         binding.btnOptD.setOnClickListener {
+            binding.btnSubmit.isEnabled = true
             defaultOptionView()
+            mSelectedOptionPosition = 4
             binding.btnOptD.background =
                 ContextCompat.getDrawable(this, R.drawable.btn_option_selected)
             binding.btnOptD.setTextColor(ContextCompat.getColor(this, R.color.black))
         }
+        binding.btnSubmit.setOnClickListener{
 
+            if (mSelectedOptionPosition == 0) {
+                mCurrentPosition ++
+                if (mCurrentPosition <= mQuestionList!!.size) {
+                    setQuestion(mCurrentPosition)
+                    binding.btnSubmit.isEnabled = false
+                }
+                else {
+                    Toast.makeText(this, "Quiz completed", Toast.LENGTH_SHORT).show()
+                }
 
-
-
-
+            }
+            else {
+                Toast.makeText(this, "$mSelectedOptionPosition", Toast.LENGTH_SHORT).show()
+                val question = mQuestionList!![mCurrentPosition - 1]
+                if (mSelectedOptionPosition != question.optTrue) {
+                    correctOptionView(mSelectedOptionPosition, R.drawable.btn_option_selected_wrong)
+                    correctOptionView(question.optTrue, R.drawable.btn_option_selected_correct)
+                }
+                else {
+                    correctOptionView(mSelectedOptionPosition, R.drawable.btn_option_selected_correct)
+                }
+                if (mCurrentPosition == mQuestionList!!.size) {
+                    binding.btnSubmit.text = "FINISH"
+                } else {
+                    binding.btnSubmit.text = "NEXT QUESTION"
+                }
+                mSelectedOptionPosition = 0
+            }
 
         }
-    private fun setQuestion() {
-        mCurrentPosition = 1
+        }
+    private fun setQuestion(position : Int = 1 ) {
+        mCurrentPosition = position
         val question = mQuestionList!![mCurrentPosition - 1]
         binding.txQuestionNumber.text = "Question $mCurrentPosition"
         binding.txQuestion.text = question.question
@@ -75,6 +114,7 @@ lateinit var binding: ActivityQuizBinding
 
     }
     private fun defaultOptionView() {
+        mSelectedOptionPosition = 0
         val options = ArrayList<Button>()
         options.add(0, binding.btnOptA)
         options.add(1, binding.btnOptB)
@@ -88,6 +128,23 @@ lateinit var binding: ActivityQuizBinding
             option.typeface = Typeface.DEFAULT
 
 
+        }
+    }
+
+    private fun correctOptionView(answer : Int, drawableView : Int) {
+        when (answer) {
+            1 -> {
+                binding.btnOptA.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            2 -> {
+                binding.btnOptB.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            3 -> {
+                binding.btnOptC.background = ContextCompat.getDrawable(this, drawableView)
+            }
+            4 -> {
+                binding.btnOptD.background = ContextCompat.getDrawable(this, drawableView)
+            }
         }
     }
 
